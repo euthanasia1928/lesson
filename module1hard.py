460438a836115f4d13a96f36b39be6daee7874d4
euthanasia1928 <mcy9000@gmail.com>
2024-05-25 02:09:43 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,15 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print(sum(grades[0]))
+print(20/5)
+print(sum(grades[1]))
+print(9/4)
+print(sum(grades[2]))
+print(16/4)
+print(sum(grades[3]))
+print(round(11/3,2))
+print(sum(grades[4]))
+print(24/5)
+spisok = {'Aaron':4.0,'Bilbo':2.25,'Johnny':4.0,'Khendrik':3.67,'Steve':4.8}
+print(spisok)
+
