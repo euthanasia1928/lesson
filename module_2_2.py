3a9a78faa2db947fc326232d31441f75b6dee3ac
euthanasia1928 <mcy9000@gmail.com>
2024-06-01 14:02:14 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,18 @@
+first = 123
+second = 456
+third = 789
+if first==second==third or first==third:
+    print(3)
+elif first==second==third:
+    print(2)
+else:
+    print(0)
+one = 42
+two = 69
+three = 42
+if one==two==three:
+    print(3)
+elif one==two==three or one==three:
+    print(2)
+else:
+    print(0)
