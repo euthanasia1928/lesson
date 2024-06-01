8c245c878e16f76dd3adead0870bff23b9ea1220
euthanasia1928 <mcy9000@gmail.com>
2024-06-01 13:37:35 +0300

    Update module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -1,15 +1,11 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-print(sum(grades[0]))
-print(20/5)
-print(sum(grades[1]))
-print(9/4)
-print(sum(grades[2]))
-print(16/4)
-print(sum(grades[3]))
-print(round(11/3,2))
-print(sum(grades[4]))
-print(24/5)
-spisok = {'Aaron':4.0,'Bilbo':2.25,'Johnny':4.0,'Khendrik':3.67,'Steve':4.8}
-print(spisok)
-
+chisla = []
+chisla.append(sum(grades[0])/len(grades[0]))
+chisla.append(sum(grades[1])/len(grades[1]))
+chisla.append(sum(grades[2])/len(grades[2]))
+chisla.append(sum(grades[3])/len(grades[3]))
+chisla.append(sum(grades[4])/len(grades[4]))
+students = sorted(students)
+itog = dict(zip(students, chisla))
+print(itog)
