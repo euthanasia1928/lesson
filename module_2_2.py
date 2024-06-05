7e92520f45a0642d1fe971d768977f860e168c0c
euthanasia1928 <mcy9000@gmail.com>
2024-06-05 13:34:36 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,18 +1,10 @@
-first = 123
-second = 456
-third = 789
-if first==second==third or first==third:
-    print(3)
-elif first==second==third:
-    print(2)
+first = int(input('Введите первое число:'))
+second = int(input('Введите второе число:'))
+third = int(input('Введите третье число:'))
+
+if first == second == third:
+    print('Вывод: 3')
+elif first == second or first == third or second == third:
+    print('Вывод: 2')
 else:
-    print(0)
-one = 42
-two = 69
-three = 42
-if one==two==three:
-    print(3)
-elif one==two==three or one==three:
-    print(2)
-else:
-    print(0)
+    print('Вывод: 0')
