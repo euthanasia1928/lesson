c0f630864be7ead4261826070a4420040166f053
euthanasia1928 <mcy9000@gmail.com>
2024-06-05 13:29:06 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,6 +1,8 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-zero = 0
+zero = 1
 while zero < len(my_list):
-    if (my_list[zero]) >= 0:
+    if (my_list[zero]) < 0:
+        break
+    if (my_list[zero]) > 0:
         print(my_list[zero])
-        zero += 1
+    zero += 1
